1f254197eea90768e0000abe470ec9709ebd6ef8
Muhammad Areeb Kazmi <54742430+MuhammadAreebKazmi@users.noreply.github.com>
2020-02-11 08:05:51 +0500

    Q3 Pg 227 of book. (May vary a few in E-Book)

---
+++ Q3 Exam style question book (Uses byRef).vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        'Question 3 of Exam style book questions
+        Dim eggnumbers, boxnumber, eggsleft As Integer
+        'Number of egss could've been input as well
+        eggnumbers = 567
+        boxnumber = 0
+        eggsleft = 0
+
+        Call EggsIntoBoxes(eggnumbers, boxnumber, eggsleft)
+
+        Console.WriteLine("Eggs = " & eggnumbers)
+        Console.WriteLine("Full boxes = " & boxnumber)
+        Console.WriteLine("Eggs leftovers = " & eggsleft)
+        Console.ReadKey()
+    End Sub
+
+    Sub EggsintoBoxes(ByVal NoOfEggs As Integer, ByRef NoOfBoxes As Integer, _
+                      ByRef EggsLeftOver As Integer)
+        NoOfBoxes = NoOfEggs \ 6
+        EggsLeftOver = NoOfEggs Mod 6
+    End Sub
+
+End Module
